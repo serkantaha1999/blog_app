da53a1a8faf82f2cb6727d7a46c33f5c185e612a
Shapoklyak <serkantaha1999@gmail.com>
2024-05-21 20:47:00 +0300

    fix imports

--- app/javascript/pages/ArticleCard/components/CommentsForm.tsx
+++ app/javascript/pages/ArticleCard/components/CommentsForm.tsx
@@ -1,11 +1,13 @@
 import React, {FC} from 'react';
+
+import {useForm} from 'react-hook-form';
+
 import Label from "../../../shared/components/Label/Label";
 import Input from "../../../shared/components/Input/Input";
-import {useForm} from "react-hook-form";
 import {messageValidator, nameValidator} from "../../../shared/utils/validationRules";
-import Button from "../../../shared/components/Button/Button";
-import Textarea from "../../../shared/components/Textarea/Textarea";
 import {articlesAPI, Comments} from "../../../app/api/api";
+import Textarea from "../../../shared/components/Textarea/Textarea";
+import Button from "../../../shared/components/Button/Button";
 
 interface FormComments {
   author: string;
@@ -27,18 +29,6 @@ const CommentsForm: FC<Props> = ({articleId, addedComments}) => {
     mode: 'onChange',
   });
 
-    const onSubmit = async (data: FormComments) => {
-        let comment = {article_id: articleId, ...data}
-        try {
-            let response = await articlesAPI.setComments(comment)
-            if (response.status === 200) {
-                addedComments(comment)
-            }
-        } catch (error) {
-            // @ts-ignore
-            window.alert("Something error! Please try again!")
-            console.log(error)
-        }
   const onSubmit = async (data: FormComments) => {
     let comment = {article_id: articleId, ...data};
     try {
