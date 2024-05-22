3dd9dea9cedaf882c788e0de5d90532317262d44
Shapoklyak <serkantaha1999@gmail.com>
2024-05-22 16:28:57 +0300

    added styles for article form page

--- app/javascript/pages/ArticleAdmin/ArticleAdmin.tsx
+++ app/javascript/pages/ArticleAdmin/ArticleAdmin.tsx
@@ -3,7 +3,9 @@ import Label from "../../shared/components/Label/Label";
 import Input from "../../shared/components/Input/Input";
 import Textarea from "../../shared/components/Textarea/Textarea";
 import {useForm} from "react-hook-form";
+import { CiImageOn } from "react-icons/ci";
 import {fileValidator, textareaValidator} from "../../shared/utils/validationRules";
+import Button from "../../shared/components/Button/Button";
 
 interface ArticleAdmitForm {
     file: string
@@ -16,22 +18,38 @@ const ArticleAdmin = () => {
     return (
         <div className={"page article-admin-page"}>
             <div className="article-admin-page__container">
+                <h1 className="home-page__title page-title">New Article</h1>
                 <form className="article-admin-page__form">
-                   <Label>
-                       <input type="file"/>
-                       <Input<ArticleAdmitForm> type={"file"} name={"file"} register={register} rules={fileValidator}/>
-                   </Label>
+                    <div className={"article-admin-page__image"}>
+                        <Label>
+                            <div className={"article-admin-page__uploader"}>
+                                <CiImageOn className={"article-admin-page__uploader-icon"} />
+                                {/*<img src={''} alt="Uploaded image" />*/}
+                            </div>
+                            <Input<ArticleAdmitForm> type={"file"} name={"file"} register={register} rules={fileValidator}/>
+                       </Label>
+                    </div>
                     <div className={"article-admin-page__block"}>
                         <Label>
-                            <input type="text" className={"input"}/>
-                            <Input<ArticleAdmitForm> type={"text"} name={"text"} register={register} rules={{}}/>
+                            <Input<ArticleAdmitForm>
+                                type={"text"}
+                                name={"text"}
+                                register={register}
+                                rules={{}}
+                                placeholder={'Write title...'}
+                            />
                         </Label>
                         <Label errors={errors.content?.message}>
-                            <textarea className={"textarea"}/>
-                            <Textarea<ArticleAdmitForm> name={"content"} register={register} rules={textareaValidator}/>
+                            <Textarea<ArticleAdmitForm>
+                                name={"content"}
+                                register={register}
+                                rules={textareaValidator}
+                                placeholder={'Write content...'}
+                            />
                         </Label>
                     </div>
                 </form>
+                <Button>Publish</Button>
             </div>
         </div>
     );
